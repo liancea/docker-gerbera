a26d7f4cf33f6062cf158f4d1bd611641d56702d
liancea <docker@mehrkoffe.in>
2019-02-28 11:57:42 +0100

    udn generation fix

--- start-gerbera.sh
+++ start-gerbera.sh
@@ -2,11 +2,14 @@
 set -ue
 
 if [[ ! -f /etc/gerbera/config.xml ]]; then
-	cp -v /var/lib/gerbera/config-dist.xml /etc/gerbera/config.xml
+    # cp -v /var/lib/gerbera/config-dist.xml /etc/gerbera/config.xml
+    gerbera --create-config >/etc/gerbera/config.xml
 fi
 
 sed -E -i -e "s/(\\s*)<account user=\"(.+)\" password=\".*\"\\/>/\\1<account user=\"\\2\" password=\"${GERBERA_WEB_PASSWORD}\"\\/>/g" /etc/gerbera/config.xml
 sed -E -i -e "s/(\\s*)<name>.*<\\/name>/\\1<name>${GERBERA_FRIENDLY_NAME}<\\/name>/g" /etc/gerbera/config.xml
 sed -E -i -e "s/(\\s*)<interface>.*<\\/interface>/\\1<interface>${GERBERA_BIND_INTERFACE}<\\/interface>/g" /etc/gerbera/config.xml
+sed -E -i -e "s/(\\s*)<home>.*<\\/home>/\\1<home>\\/var\\/lib\\/gerbera<\\/home>/g" /etc/gerbera/config.xml
+
 
 exec /usr/local/bin/gerbera -c /etc/gerbera/config.xml "$@"
